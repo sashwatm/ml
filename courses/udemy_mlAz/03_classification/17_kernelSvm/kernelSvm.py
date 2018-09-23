461936504ea4baeb47d81beb2331bbff3e7ba372
Sashwat Mishra <sashwatmishra@gmail.com>
2018-09-23 15:59:43 -0400

    Done with Kernel SVm, looking at rbf kernel and how it elevates non linear separator problems to higher dimension and classifies non-linearly.

---
+++ courses/udemy_mlAz/03_classification/17_kernelSvm/kernelSvm.py
@@ -0,0 +1,55 @@
+# Kernel Support Vector Machine (SVM)
+
+# Importing libs
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as pltclr
+import pandas as pd
+import sklearn.preprocessing as sklp
+import sklearn.model_selection as sklcv
+import sklearn.svm as sksvm
+import sklearn.metrics as sklmet
+
+# Read in data
+dataset = pd.read_csv("Social_Network_Ads.csv")
+X = dataset.iloc[:,[2,3]].values
+y = dataset.iloc[:,4].values
+
+# Split dataset into training and test datasets ot validate ML model
+XTrain,XTest,yTrain,yTest = sklcv.train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+scX = sklp.StandardScaler()
+XTrain = scX.fit_transform(XTrain)
+XTest = scX.transform(XTest)
+
+# Create classifier
+classifier = sksvm.SVC(kernel = 'rbf', random_state = 0)
+classifier.fit(XTrain,yTrain)
+
+# Predicting test set results
+yPred = classifier.predict(XTest)
+print(yPred)
+
+# Creating confusion matrix
+cm = sklmet.confusion_matrix(yTest, yPred)
+print(cm)
+
+# Visualizing the Training set results
+XSet, ySet = XTrain, yTrain
+X1, X2 = np.meshgrid(
+         np.arange(start = XSet[:,0].min() - 1, stop = XSet[:,0].max() + 1, step = 0.01), \
+         np.arange(start = XSet[:,1].min() - 1, stop = XSet[:,1].max() + 1, step = 0.01) \
+)
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = pltclr.ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(ySet)):
+    plt.scatter(XSet[ySet == j, 0], XSet[ySet == j, 1],
+                c = pltclr.ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM Classification - Ad Campaign (Training set)')
+plt.xlabel('Age [years]')
+plt.ylabel('Estimated Salary [USD]')
+plt.legend()
+plt.show()
